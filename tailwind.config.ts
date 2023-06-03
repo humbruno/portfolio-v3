0d45d2302560fb94322b3dfb9528192c0b1ce04f
Bruno Santos <>
2023-06-03 15:20:00 +0100

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.css",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: [
+          "Fira Code",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
