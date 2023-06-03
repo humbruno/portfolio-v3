de527bf42987bc40644266a33481c4577386db54
Bruno Santos <>
2023-06-03 18:21:07 +0100

    chore: adjust mobile styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ function Home() {
 
   return (
     <Layout>
-      <AnimateOnAppear className="m-auto max-w-4xl px-4">
+      <AnimateOnAppear className="mt-36 max-w-4xl px-4 sm:m-auto">
         <h1 className="h1 title mb-4">{title}</h1>
         <h2 className="h3 subtitle mb-4">{description}</h2>
       </AnimateOnAppear>
